06646c1ce1eeabc5579e78024aa54d936e0d027e
gendronx <gendronx@educ.college-montreal.qc.ca>
2024-09-26 10:11:06 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+"""
+Xavier Gendron
+404
+ce programme fait deviné un nombre aléatoire
+"""
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+
+intervalle = int(input("Le programme choisira un nombre entre 0 et: "))
+nb = random.randint(0, intervalle)
+print(f"J'ai choisi un nombre entre 0 et {intervalle}, à vous de le deviner \nBonne chance")
+
+nbessai = 0
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def nbmystere(nb_essai):
+    essai = int(input("Entrez un nombre: "))
+
+    if essai > nb:
+        print("Le nombre que vous avez entrer est trop grand")
+        nb_essai += 1
+        nbmystere(nb_essai)
+
+    elif essai < nb:
+        print("Le nombre que vous avez entrer est trop petit")
+        nb_essai += 1
+        nbmystere(nb_essai)
+
+    elif essai == nb:
+        print("Bravo, vous avez trouver le nombre secret")
+        nb_essai += 1
+        print("Vous avez réussi en", nb_essai, "essai")
+        newgame = input("Voulez-vous faire une autre parti y/n: ")
+
+        if newgame == "y":
+            nb_essai = 0
+            nbmystere(nb_essai)
+
+        elif newgame == "n":
+            print("Merci d'avoir joué")
+
+        else:
+            print("Cette réponse na pas pu être accepté")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+nbmystere(nbessai)
